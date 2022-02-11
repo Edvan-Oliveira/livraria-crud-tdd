490a247126be6b8408f33a856ebbc273a789c26c
Edvan Oliveira <edvan.oliveiract@gmail.com>
2022-02-11 20:16:09 -0300

    Iniciado

---
+++ src/main/java/livraria/LivrariaCrudTddApplication.java
@@ -0,0 +1,13 @@
+package livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaCrudTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaCrudTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/livraria/LivrariaCrudTddApplicationTests.java
@@ -0,0 +1,13 @@
+package livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaCrudTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
