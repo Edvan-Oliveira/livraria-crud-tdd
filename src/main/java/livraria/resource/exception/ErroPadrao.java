b953accdde5d32ced6f7a3dc2232dd0da0d9db8b
Edvan Oliveira <edvan.oliveiract@gmail.com>
2022-02-14 16:21:46 -0300

    Refatorando manipulador de exceções

--- src/main/java/livraria/resource/exception/ErroPadrao.java
+++ src/main/java/livraria/resource/exception/ErroPadrao.java
@@ -1,5 +1,6 @@
 package livraria.resource.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -11,5 +12,6 @@ public class ErroPadrao {
     private String caminho;
     private int status;
     private String mensagem;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime momento;
 }
--- src/main/java/livraria/resource/exception/ManipuladorException.java
+++ src/main/java/livraria/resource/exception/ManipuladorException.java
@@ -14,25 +14,15 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @ControllerAdvice
 public class ManipuladorException {
 
-    @ExceptionHandler(ISBNDuplicadoException.class)
-    public ResponseEntity<ErroPadrao> isbnDuplicadoException(ISBNDuplicadoException excecao,
-                                                             HttpServletRequest requisicao) {
+    @ExceptionHandler({
+            ISBNDuplicadoException.class,
+            ConteudoNaoEncontradoException.class
+    })
+    public ResponseEntity<ErroPadrao> isbnDuplicadoException(RuntimeException excecao, HttpServletRequest requisicao) {
         ErroPadrao erro = ErroPadrao.builder()
                 .caminho(requisicao.getRequestURI())
                 .status(BAD_REQUEST.value())
                 .mensagem(excecao.getMessage())
-                .momento(LocalDateTime.now())
-                .build();
-        return ResponseEntity.status(BAD_REQUEST).body(erro);
-    }
-
-    @ExceptionHandler(ConteudoNaoEncontradoException.class)
-    public ResponseEntity<ErroPadrao> conteudoNaoEncontradoException(ConteudoNaoEncontradoException exception,
-                                                                     HttpServletRequest requisicao) {
-        ErroPadrao erro = ErroPadrao.builder()
-                .caminho(requisicao.getRequestURI())
-                .status(BAD_REQUEST.value())
-                .mensagem(exception.getMessage())
                 .momento(LocalDateTime.now())
                 .build();
         return ResponseEntity.status(BAD_REQUEST).body(erro);
