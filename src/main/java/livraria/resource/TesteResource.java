a494275358f397bf4824808c6c40b5ac1bf0c99a
Edvan Oliveira <edvan.oliveiract@gmail.com>
2022-04-08 17:49:00 -0300

    Testando integração Travis Heroku

---
+++ src/main/java/livraria/resource/TesteResource.java
@@ -0,0 +1,15 @@
+package livraria.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteResource {
+
+    @GetMapping
+    public String teste() {
+        return "Olá, Mundo!";
+    }
+}
